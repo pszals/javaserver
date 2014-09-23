302ad4e80cedebb58f9a6e570f6ba92a9f975310
Philip Szalwinski <pszalwinski@gmail.com>
2014-09-23 10:46:15 -0700

    Remove compilation bug

--- IdeaProjects/PlayingWithBufferedReader/src/Main.java
+++ IdeaProjects/PlayingWithBufferedReader/src/Main.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StringReader;
 
 /**
@@ -15,7 +14,7 @@ public class Main {
         String text = "this is some text\r\n";
         BufferedReader br = new BufferedReader(new StringReader(text));
         br.readLine();
-        br.
+
 
     }
 }
